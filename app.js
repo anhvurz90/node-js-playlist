8bbe0881d720ade45ca32dad1eb4210a2b99a933
anhvurz90 <anhvurz90@gmail.com>
2016-12-31 21:48:57 +0700

    Add setTimeout & setInterval in app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("hey ninjas");
+
+setTimeout(function() {
+  console.log("2 seconds have passed.");
+}, 2000);
+
+var time = 0;
+setInterval(function() {
+  time += 2;
+  console.log(time  + " seconds have passed");
+}, 2000);
