c511c6df18691a92b91ee695e9587ba8c04009b3
anhvurz90 <anhvurz90@gmail.com>
2016-12-31 21:52:39 +0700

    Add clearInterval() in app.js

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ setInterval(function() {
   time += 2;
   console.log(time  + " seconds have passed");
 }, 2000);
+
+var time1 = 0;
+var timer = setInterval(function() {
+  time1 += 3;
+  console.log(time1 + " seconds have passed...");
+  if (time1 > 10) {
+    clearInterval(timer);
+  }
+}, 2000);
