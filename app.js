4aa4792eff3caf69f26bfa77c903b20c32cb6a07
anhvurz90 <anhvurz90@gmail.com>
2016-12-31 21:57:50 +0700

    Add __dirname & __filename in app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 console.log("hey ninjas");
-
+//--------------------------------------------------
 setTimeout(function() {
   console.log("2 seconds have passed.");
 }, 2000);
-
+//--------------------------------------------------
 var time = 0;
 setInterval(function() {
   time += 2;
   console.log(time  + " seconds have passed");
 }, 2000);
-
+//--------------------------------------------------
 var time1 = 0;
 var timer = setInterval(function() {
   time1 += 3;
@@ -18,3 +18,6 @@ var timer = setInterval(function() {
     clearInterval(timer);
   }
 }, 2000);
+//--------------------------------------------------
+console.log(__dirname);
+console.log(__filename);
