1fa6525d0e6c8b04a3d51dffea5ad2719695520d
anhvurz90 <anhvurz90@gmail.com>
2017-01-05 22:25:54 +0700

    Add #15.Write Streams

--- app14.js
+++ app14.js
@@ -1,6 +1,8 @@
 var fs = require("fs");
 var myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
+var myWriteStream = fs.createWriteStream("writeMe.txt");
 
 myReadStream.on("data", function(chunk) {
   console.log("New chunk received:\n" + chunk);
+  myWriteStream.write(chunk);
 });
