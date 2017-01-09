f0fe89f322c5ea580061902244d318448dbe54a4
anhvurz90 <anhvurz90@gmail.com>
2017-01-09 22:54:42 +0700

    Add #23.Express

---
+++ app23.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+  res.send("This is the home page");
+});
+
+app.get("/contact", function(req, res) {
+  res.send("This is the contact page");
+});
+
+app.listen(3000);
