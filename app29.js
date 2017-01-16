517435f4f5353a5d8e0609bc5efa0cb86a13721f
anhvurz90 <anhvurz90@gmail.com>
2017-01-16 22:19:15 +0700

    Add #29.Query Strings

---
+++ app29.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use("/assets", express.static("assets"));
+
+app.get("/", function(req, res) {
+  res.render("index28");
+});
+
+app.get("/contact", function(req, res) {
+  console.log(req.query);
+  res.render("contact29", {qs: req.query});
+});
+
+app.get("/profile/:name", function(req, res) {
+  var data = {age: 29, job: "ninja", hobbies: ["eating", "fighting", "fishing"]};
+  res.render("profile27", {person: req.params.name, data: data});
+});
+
+app.listen(3000);
